a32c9e135aaad53f6cb01b3569028bd119bf89b4
medi2015 <mehdi@soft-it.no>
2026-05-12 13:54:05 +0200

    update main.rs upadter

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,7 @@ use tauri_plugin_autostart::MacosLauncher;
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_updater::init())
+.plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_autostart::init(MacosLauncher::LaunchAgent, Some(vec!["--silent"])))
